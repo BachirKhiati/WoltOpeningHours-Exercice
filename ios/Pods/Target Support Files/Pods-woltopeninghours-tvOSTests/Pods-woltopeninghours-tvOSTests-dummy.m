8992048fd6f9bc3990293d2c7faea8736d125717
Bachir Khiati <bachir.khiati@gmail.com>
2020-02-19 07:19:41 +0200

    pods

---
+++ ios/Pods/Target Support Files/Pods-woltopeninghours-tvOSTests/Pods-woltopeninghours-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_woltopeninghours_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_woltopeninghours_tvOSTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-woltopeninghoursTests/Pods-woltopeninghoursTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_woltopeninghoursTests : NSObject
+@end
+@implementation PodsDummy_Pods_woltopeninghoursTests
+@end
